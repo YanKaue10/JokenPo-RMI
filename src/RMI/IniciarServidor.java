67fdd5609b0c3be96fef14a60aa32fa7a4b983f2
Yan Kauê <147212233+YanKaue10@users.noreply.github.com>
2026-05-22 07:46:47 -0300

    Adicionei o ip da máquina para testar o servidor...

--- src/RMI/IniciarServidor.java
+++ src/RMI/IniciarServidor.java
@@ -6,7 +6,7 @@ import java.rmi.registry.Registry;
 public class IniciarServidor {
     public static void main(String[] args) {
         try {
-            System.setProperty("java.rmi.server.hostname", "conectar o endereço ip da máquina");
+            System.setProperty("java.rmi.server.hostname", "172.16.19.2");
             ServidorJokenPo servidorJokenPo = new ServidorJokenPo();
             Registry registry = LocateRegistry.createRegistry(1099);
             registry.rebind("JokenPo", servidorJokenPo);
